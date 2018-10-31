01ddba9842736957dee8573ebe84140771907fe6
Marcin Cukrowski <marcin.cukrowski@balluff.pl>
2018-10-31 11:55:25 +0100

    Add project files.

---
+++ 20181031_SystemTimer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20181031_SystemTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
