4201e47e8cfd9bfb125aebf3207f01ae15af2a4a
Marcin Cukrowski <marcin.cukrowski@balluff.pl>
2018-10-31 13:05:12 +0100

    event handler timer co 1000ms

--- 20181031_SystemTimer/Program.cs
+++ 20181031_SystemTimer/Program.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace _20181031_SystemTimer
-{
+{ 
     class Program
     {
         static void Main(string[] args)
         {
+            Timer T1 = new Timer();
+
+            T1.Start();
+
+            T1.Interval = 1000;
+                       
+            T1.Elapsed += T1_Elapsed;
+
+            Console.ReadKey();
+        }
+
+        private static void T1_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.Clear();
+
+            Console.WriteLine($"Actual time: {DateTime.Now}");
         }
     }
 }
