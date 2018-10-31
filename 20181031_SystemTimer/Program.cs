56894dfa8f410e72cb228b3ecb83aee078c1d54a
Marcin Cukrowski <marcin.cukrowski@balluff.pl>
2018-10-31 13:48:40 +0100

    dodano event handler dla dzialania na folderze

--- 20181031_SystemTimer/Program.cs
+++ 20181031_SystemTimer/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
+using System.IO;
+
 
 namespace _20181031_SystemTimer
 { 
@@ -13,20 +15,37 @@ namespace _20181031_SystemTimer
         {
             Timer T1 = new Timer();
 
-            T1.Start();
+     //       T1.Start();
 
             T1.Interval = 1000;
                        
             T1.Elapsed += T1_Elapsed;
+            FileSystemWatcher check = new FileSystemWatcher(@"D:\C#\");
+            check.EnableRaisingEvents = true;
+
+            check.Created += EventHandler;
+            check.Deleted += EventHandler;
+            check.Changed += EventHandler;
 
             Console.ReadKey();
+                    
+        }
+
+        private static void EventHandler(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Zmiana w folderze: {e.Name}, {e.ChangeType}");
+            
         }
 
         private static void T1_Elapsed(object sender, ElapsedEventArgs e)
         {
             Console.Clear();
-
+            
             Console.WriteLine($"Actual time: {DateTime.Now.ToString("HH:mm:ss")} ");
+            Console.WriteLine("###################################");
+
+            
+           
         }
     }
 }
